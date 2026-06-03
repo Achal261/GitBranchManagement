520b302337dcaa52d6bcf14b926ba4149815edf0
Achal261 <achalkurundwade26104@gmail.com>
2026-06-03 08:53:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function showMessage() {
     document.getElementById("output").innerHTML =
         "Welcome to Online Shopping System";
 }
+
+function addToCart() {
+    alert("Product Added To Cart");
+}
